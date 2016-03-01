855145bbd902ffe079c8020ced049e8cb071859b
jeinglis <jeinglis@ucalgary.ca>
2016-02-29 20:01:12 -0700

    added some basic client and server classes

---
+++ client_connect.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.net.*;
+import java.nio.*;
+import java.nio.channels.*;
+import java.nio.charset.*;
+
+
+public class client_connect {
+
+}
---
+++ server_connect.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.lang.reflect.Executable;
+import java.net.*;
+import java.nio.*;
+import java.nio.channels.*;
+import java.nio.charset.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+
+
+public class server_connect {
+
+}
